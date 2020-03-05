0224642b716d654d3e8e0ee2d7f90f959fef8323
Williams <williams@Mauricio.lan>
2020-03-05 10:22:02 -0300

    first Commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,22 +3,22 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
+  beforeEach(async(() => { 
     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule
       ],
-      declarations: [
-        AppComponent
+      declarations: [ 
+        AppComponent 
       ],
     }).compileComponents();
   }));
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    const app = fixture.componentInstance; 
     expect(app).toBeTruthy();
-  });
+  }); 
 
   it(`should have as title 'ModuloInfo'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -33,3 +33,4 @@ describe('AppComponent', () => {
     expect(compiled.querySelector('.content span').textContent).toContain('ModuloInfo app is running!');
   });
 });
+ 
